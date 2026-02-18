5e088b48c36dffed63dcaaf1408c71f1b25eaea9
Cody Rutscher <codyrutscher@gmail.com>
2026-02-18 02:03:10 -0600

    Fix: Convert postcss config to CommonJS to avoid __dirname error

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('=== MIDDLEWARE LOG ===');
-  console.log('URL:', request.url);
-  console.log('Path:', request.nextUrl.pathname);
-  console.log('Method:', request.method);
-  console.log('Headers:', Object.fromEntries(request.headers.entries()));
-  console.log('======================');
-  
+  // Simple logging
+  console.log('Request:', request.nextUrl.pathname);
   return NextResponse.next();
 }
 
